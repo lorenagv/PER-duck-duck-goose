fa64ea5d88f77018ad7f19d4b11d35b656ee6898
logavi <logavi20@gmail.com>
2019-01-21 16:02:48 +0100

    mi commit
    mi commit
    
    mi commit

---
+++ juego_ddg.py
@@ -0,0 +1,14 @@
+
+lista_nombres = ["Ines", "Paula", "Carlos", "Alberto", "Carol"]
+numero = int(input("Introduce un numero: "))
+
+def ddg(lista_nombres, numero):
+    if numero < len(lista_nombres):
+        print(lista_nombres[numero])
+        return
+
+    else:
+        print("Error.")
+
+ddg(lista_nombres, numero)
+
